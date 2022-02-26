246896d2763cb15ea1b7fe23b9bd225fe6fe6e74
Angel Concha Layme <aconchal@unsa.edu.pe>
2022-02-26 18:08:52 -0500

    Create Principal.java

---
+++ Principal.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import javax.swing.event.ChangeListener;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class Principal extends JFrame implements ActionListener, ChangeListener {
+   
+    public Principal(){
+        
+    }
+
+    public static void main(String[] args) {
+        
+    }
+
+}
