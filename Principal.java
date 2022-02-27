e700da3a121501c2e5cd81aad4a74ff7a11c18e3
Angel Concha Layme <aconchal@unsa.edu.pe>
2022-02-27 00:21:45 -0500

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -4,10 +4,10 @@ import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Principal extends JFrame implements ActionListener, ChangeListener {
+public class Principal extends JFrame implements ActionListener{
    
     public Principal(){
-        
+
     }
 
     public static void main(String[] args) {
